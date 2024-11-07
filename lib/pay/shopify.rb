57e70d3fea8948917edc295cc805bb990b993e3d
uurcank <uurcan@pasilobus.com>
2024-11-07 07:09:48 -0800

    Update shopify.rb

--- lib/pay/shopify.rb
+++ lib/pay/shopify.rb
@@ -1,11 +1,6 @@
 module Pay
-    module Shopify
-      autoload :Billable, "pay/shopify/billable"
-      autoload :Charge, "pay/shopify/charge"
-      autoload :Error, "pay/shopify/error"
-      autoload :PaymentMethod, "pay/shopify/payment_method"
-      autoload :Subscription, "pay/shopify/subscription"
-      autoload :Merchant, "pay/shopify/merchant"
+  module Shopify
+    class Error < Pay::Error
     end
   end
-  
+end
